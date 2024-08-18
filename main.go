8456eaf312ad2b4032186d15e340affe55618e9e
Morgan Helton <mhelton@gmail.com>
2024-08-18 10:31:51 +0300

    change text and color

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("devusb is Awesome???", "larry3d", "red", true)
+	myFigure := figure.NewColorFigure("devusb is Awesome...", "larry3d", "brown", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
